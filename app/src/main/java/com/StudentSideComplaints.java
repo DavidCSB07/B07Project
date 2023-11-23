04f309f81d186a526e2a46d5194a4c2436090473
Hillary Tse <hillaryy.tse@gmail.com>
2023-11-23 03:06:08 -0500

    Added fragment

---
+++ app/src/main/java/com/StudentSideComplaints.java
@@ -0,0 +1,16 @@
+package com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.b07project.R;
+
+public class StudentSideComplaints extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_side_complaints);
+    }
+}
