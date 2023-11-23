7771770277efb4eb258c6c8352c0e6a3d681422a
yimingzhong1002 <lynnzhong1002@gmail.com>
2023-11-23 17:59:49 -0500

    FrontPage done

---
+++ app/src/main/java/com/example/finalmerge/PostAnn/PostAnnouncement.java
@@ -0,0 +1,15 @@
+package com.example.finalmerge.PostAnn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalmerge.R;
+
+public class PostAnnouncement extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_announcement);
+    }
+}
