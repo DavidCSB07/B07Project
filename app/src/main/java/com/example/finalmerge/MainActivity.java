01a49e789a6d9d2c3d2893b8f4c0ecfa1700b958
Hillary Tse <hillaryy.tse@gmail.com>
2023-11-24 18:47:15 -0500

    Completed Student side complaints form, ready to test

--- app/src/main/java/com/example/finalmerge/MainActivity.java
+++ app/src/main/java/com/example/finalmerge/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.finalmerge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -55,19 +63,44 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 //check validation
 
-                if (TextUtils.isEmpty(editEmail.getText().toString())) {
+                Editable editable=editEmail.getText();
+                if (editable==null || TextUtils.isEmpty(editable.toString())) {
                     Toast.makeText(MainActivity.this, "Please type email", Toast.LENGTH_SHORT).show();
                 }
 
-                if (TextUtils.isEmpty(editSubject.getText().toString())) {
+                editable=editSubject.getText();
+                if (editable==null || TextUtils.isEmpty(editable.toString())) {
                     Toast.makeText(MainActivity.this, "Subject cannot be empty",Toast.LENGTH_SHORT).show();
                 }
 
-                if (TextUtils.isEmpty(editComplaint.getText().toString())) {
+                editable=editComplaint.getText();
+                if (editable==null || TextUtils.isEmpty(editComplaint.toString())) {
                     Toast.makeText(MainActivity.this, "Complaint cannot be empty",Toast.LENGTH_SHORT).show();
                 }
 
+                FirebaseDatabase database=FirebaseDatabase.getInstance();
 
+                DatabaseReference ref=database.getReference();
+
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Object value=dataSnapshot.getValue();
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(MainActivity.this, "Failed to read value", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                ref.child("Complaints").child(editEmail.getText().toString()).child("Email").setValue(editEmail.getText().toString());
+                ref.child("Complaints").child(editEmail.getText().toString()).child("Subject").setValue(editSubject.getText().toString());
+                ref.child("Complaints").child(editEmail.getText().toString()).child("Complaint").setValue(editComplaint.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Thank you for your feedback", Toast.LENGTH_SHORT).show();
             }
         });
 
