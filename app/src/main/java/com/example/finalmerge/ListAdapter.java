a34a3f2f046bf5787b0254da9429bf46cee8465b
jhwlam <jhw.lam@mail.utoronto.ca>
2023-11-25 15:58:26 -0500

    General front end is done, backend has some issues (need to sync with Lynn's code)

--- app/src/main/java/com/example/finalmerge/ListAdapter.java
+++ app/src/main/java/com/example/finalmerge/ListAdapter.java
@@ -1,1 +1,44 @@
+package com.example.finalmerge;
 
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import java.util.ArrayList;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ListAdapter extends ArrayAdapter {
+
+    private Activity mContext;
+    List<Announcements> announcements;
+    public ListAdapter(Activity mContext, List<Announcements> announcementList){
+        super(mContext, R.layout.list_item, announcementsList);
+        this.mContext = mContext;
+        this.announcementList = announcementList;
+    }
+
+    @NonNull
+    @Override
+    public View getview(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+
+        LayoutInflater inflater = mContext.getLayoutInflater();
+        View listItemView = inflater.inflate(R.layout.list_item, null, true);
+
+        TextView subject = listItemView.findViewById(R.id.subject);
+        TextView ann_description = listItemView.findViewById(R.id.ann_description);
+        TextView id = listItemView.findViewById(R.id);
+
+        Announcements announcements = announcementList.get(position);
+
+        subject.setText(announcements.getTitle);
+        subject.setText(announcements.getDescription);
+        subject.setText(announcements.getID);
+
+        return listItemView;
+    }
+}
--- app/src/main/java/com/example/finalmerge/ViewAnnouncements.java
+++ app/src/main/java/com/example/finalmerge/ViewAnnouncements.java
@@ -1,14 +1,48 @@
 package com.example.finalmerge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewAnnouncements extends AppCompatActivity {
+
+    ListView listView;
+    List<Announcements> announcementList;
+
+    DatabaseReference announcementDbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_announcements);
+
+        listView = findViewByID(R.id.listView);
+        announcementsList = new ArrayList<>();
+
+        announcementDbRef = FirebaseDatabase.getInstance().getReference("https://cscb0-13f6b-default-rtdb.firebaseio.com/");
+
+        announcementDbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot){
+                announcementList.clear();
+
+                for (DataSnapshot announcementDataSnap : dataSnapshot.getChildren()){
+                    Announcements announcements = announcementDatasnap.getValue(Announcements.class);
+                    announcmentsList.add(announcements);
+                    }
+                    ListAdapter adapter = new ListAdapter(RetrieveDataActivity.this, announcementList);
+                    listView.setAdapter(adapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError){
+
+            }
+        });
+
     }
 }
