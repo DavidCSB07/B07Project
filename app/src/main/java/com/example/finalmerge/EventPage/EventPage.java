ca40f5ad089ee8a4ecc792c90ff59fa22edabdc8
DavidCSB07 <dwu.wu@mail.utoronto.ca>
2023-11-27 19:51:38 -0500

    fixing bug

--- app/src/main/java/com/example/finalmerge/EventPage/EventPage.java
+++ app/src/main/java/com/example/finalmerge/EventPage/EventPage.java
@@ -1,5 +1,6 @@
 package com.example.finalmerge.EventPage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,18 +10,31 @@ import android.os.Bundle;
 import com.example.finalmerge.EventPage.Adapter.EventAdapter;
 import com.example.finalmerge.EventPage.Model.Event;
 import com.example.finalmerge.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EventPage extends AppCompatActivity {
 
+    RecyclerView recyclerview;
+    EventAdapter eventAdapter;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    List<Event> eventsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_recycleview);
 
-        RecyclerView recyclerview = findViewById(R.id.recycleView);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("ScheduleEvent");
+        recyclerview = findViewById(R.id.recycleView);
 
         List<Event> items = new ArrayList<Event>();
         items.add(new Event("David", "David@gmail.com"));
@@ -32,4 +46,31 @@ public class EventPage extends AppCompatActivity {
         recyclerview.setLayoutManager(new LinearLayoutManager(this));
         recyclerview.setAdapter(new EventAdapter(getApplicationContext(), items));
     }
+
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                eventsList = new ArrayList<>();
+                for(DataSnapshot Eventsnapshot : snapshot.getChildren()){
+
+                    Event event = Eventsnapshot.getValue(Event.class);
+                    eventsList.add(event);
+                }
+
+                eventAdapter = new EventAdapter(getApplicationContext(), eventsList);
+                recyclerview.setAdapter(eventAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+       }
+     */
 }
