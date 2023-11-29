f48b11d1bd05583a097d199b66f1448acfaf888e
yimingzhong1002 <lynnzhong1002@gmail.com>
2023-11-29 14:13:07 -0500

    Created class UserInfo for Login Page to keep track UserInformation

---
+++ app/src/main/java/com/example/finalmerge/LoginPage/Model/UserInfo.java
@@ -0,0 +1,15 @@
+package com.example.finalmerge.LoginPage.Model;
+
+public class UserInfo {
+
+    String Uid;
+
+    String email;
+
+    String password;
+
+    String userName;
+
+    String userType
+
+}
