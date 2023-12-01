1c3af7e9fe05bb8adc3d2a81c601107738706d71
DavidCSB07 <dwu.wu@mail.utoronto.ca>
2023-12-01 18:22:54 -0500

    Fixing bug

--- app/src/main/java/com/example/finalmerge/EventPage/Holder/EventViewHolder.java
+++ app/src/main/java/com/example/finalmerge/EventPage/Holder/EventViewHolder.java
@@ -1,7 +1,6 @@
 package com.example.finalmerge.EventPage.Holder;
 
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
--- app/src/main/java/com/example/finalmerge/RSVP/EventDetail.java
+++ app/src/main/java/com/example/finalmerge/RSVP/EventDetail.java
@@ -1,16 +1,21 @@
 package com.example.finalmerge.RSVP;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.finalmerge.R;
 
 public class EventDetail extends AppCompatActivity {
 
+    TextView title, description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_binding_page);
+
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
     }
 }
