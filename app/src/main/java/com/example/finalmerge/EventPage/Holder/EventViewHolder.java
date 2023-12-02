12b68da3fbe2065291b73c0e5df97da9028ae739
DavidCSB07 <dwu.wu@mail.utoronto.ca>
2023-12-02 17:30:28 -0500

    passed information to other intent using putExtra for RSVP

--- app/src/main/java/com/example/finalmerge/EventPage/Holder/EventViewHolder.java
+++ app/src/main/java/com/example/finalmerge/EventPage/Holder/EventViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.finalmerge.EventPage.Holder;
 
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,9 +13,13 @@ public class EventViewHolder extends RecyclerView.ViewHolder{
 
     //public ImageView imageView;
     public TextView descriptionView, titleView;
+
+    public RelativeLayout layout;
     public EventViewHolder(@NonNull View itemView) {
         super(itemView);
         titleView = itemView.findViewById(R.id.title);
         descriptionView = itemView.findViewById(R.id.description);
+        layout = itemView.findViewById(R.id.layout);
+
     }
 }
