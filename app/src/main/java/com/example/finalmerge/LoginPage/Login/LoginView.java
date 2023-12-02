b23ea2adc2b7011046e0c7c8f26b11e27fa99c3c
jhwlam <jhw.lam@mail.utoronto.ca>
2023-12-02 11:48:01 -0500

    Revert "Added login view interface"
    This reverts commit 68821e1c2370451343d9b250ceb7402636e419fb.

--- app/src/main/java/com/example/finalmerge/LoginPage/Login/LoginView.java
+++ app/src/main/java/com/example/finalmerge/LoginPage/Login/LoginView.java
@@ -1,7 +1,5 @@
 package com.example.finalmerge.LoginPage.Login;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -27,32 +25,126 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class LoginView implements LoginViewInterface {
-    private ProgressBar progressBar;
-    private Context context;
+public class LoginView extends AppCompatActivity {
 
-    public LoginView(Context context, ProgressBar progressBar){
-        this.context = context;
-        this.progressBar = progressBar;
-    }
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonLog;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar; //id of 'stuff' in layout
+    TextView StudentRegister;
+    TextView AdminRegister;
+    DatabaseReference LoginDbRef;
 
+    //Check if acc is signed in
+    /*
     @Override
-    public void showProgress() {
-
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), homePage.class);
+            startActivity(intent);
+            finish();
+        }
     }
-
+     */
     @Override
-    public void hideProgress() {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage_login);
 
-    }
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonLog = findViewById(R.id.login_button);
+        progressBar = findViewById(R.id.progressBar);
+        StudentRegister = findViewById(R.id.StudentRegister);
+        AdminRegister = findViewById(R.id.AdminRegister);
 
-    @Override
-    public void showError(String message) {
+        mAuth = FirebaseAuth.getInstance();
 
-    }
+        StudentRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), com.example.finalmerge.LoginPage.StudentRegister.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
-    @Override
-    public void navigateToHomePage(String userType) {
+        AdminRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), com.example.finalmerge.LoginPage.AdminRegister.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
+
+
+        buttonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = editTextEmail.getText().toString();
+                password = editTextPassword.getText().toString();
+                progressBar.setVisibility(View.VISIBLE);
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginView.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginView.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    LoginDbRef = FirebaseDatabase.getInstance().getReference();
+                                    String uid = task.getResult().getUser().getUid();
+                                    LoginDbRef.child("RegisterInfo").child(uid).child("userType").addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                            int userType = snapshot.getValue(Integer.class);
+                                            if(userType == 0) {
+                                                Toast.makeText(getApplicationContext(), "login Successful as Admin", Toast.LENGTH_SHORT).show();
+                                                Intent intent = new Intent(getApplicationContext(), homePage.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                            else if(userType == 1) {
+                                                Toast.makeText(getApplicationContext(), "login Successful as Student", Toast.LENGTH_SHORT).show();
+                                                Intent intent = new Intent(getApplicationContext(), homePage.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                            else {
+                                                Toast.makeText(getApplicationContext(), "Please Delete account something went wrong", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+
+                                        }
+                                    });
+
+
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginView.this, "LoginView fail.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
