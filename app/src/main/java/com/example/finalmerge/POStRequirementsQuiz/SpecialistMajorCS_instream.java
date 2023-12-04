cd050e44f7ffdc2de20ff720a9bcef43a52a054e
KehuLi <kehu.li@mail.utoronto.ca>
2023-12-03 22:44:12 -0500

    Kehu - POSt_ver2 --> created activity_specialist_major_cs_instream --> code in main.xml --> code in QuestionAnswer.java --> fixed bugs --> problem: color b&w --> next step:     --> build quiz for other category     --> fix color b&w

---
+++ app/src/main/java/com/example/finalmerge/POStRequirementsQuiz/SpecialistMajorCS_instream.java
@@ -0,0 +1,16 @@
+package com.example.finalmerge.POStRequirementsQuiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalmerge.R;
+
+public class SpecialistMajorCS_instream extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specialist_major_cs_instream);
+    }
+}
