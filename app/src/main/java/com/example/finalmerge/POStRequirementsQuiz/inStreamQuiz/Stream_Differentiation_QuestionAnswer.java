843bf4dae008902f628d90efea3d24bd674552fa
KehuLi <kehu.li@mail.utoronto.ca>
2023-12-04 22:32:01 -0500

    Kehu - POSt_ver2 --> created Stream_Differentiation --> created Stream_Differentiation_QuestionAnswer --> fixed bugs --> next step:     --> merge branch, activate "Home" button     --> build and finish quiz for each program category

---
+++ app/src/main/java/com/example/finalmerge/POStRequirementsQuiz/inStreamQuiz/Stream_Differentiation_QuestionAnswer.java
@@ -0,0 +1,11 @@
+package com.example.finalmerge.POStRequirementsQuiz.inStreamQuiz;
+
+public class Stream_Differentiation_QuestionAnswer {
+    public static String[] question = {
+            "Which stream are you applying to?"
+    };
+
+    public static String[][] choices = {
+            {"CS", "Math", "Stats"},
+    };
+}
